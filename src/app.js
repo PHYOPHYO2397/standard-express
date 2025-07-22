6157b9d6aee9b3f004a35c43a543b7061993b84a
PHYOPHYO2397 <phyophyomyatthu3@gmail.com>
2025-07-23 01:18:28 +0900

    cors added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+  })
+);
+
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { app } from "./app.js";
+import dotenv from "dotenv";
 
-const PORT = 3000;
+dotenv.config({
+  path: ".env",
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Connected to standard express server");
+  console.log("Connected to standard express server" + PORT);
 });
